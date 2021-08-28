88e87643de043c590b6130f2a8041685c49fe9ce
Ryan Chen <yu-an.chen@verizonmedia.com>
2021-08-28 16:42:10 +0800

    feat: activate wallet options

--- src/components/Header/WalletModal.jsx
+++ src/components/Header/WalletModal.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
+import { useWeb3React } from '@web3-react/core'
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Card, Fade, Modal, Paper, Typography } from '@material-ui/core';
+import { Box, Card, CircularProgress, Fade, Modal, Paper, Typography } from '@material-ui/core';
 import SettingsEthernetOutlinedIcon from '@material-ui/icons/SettingsEthernetOutlined';
 // connector
 import { supportedWallet } from '../../connectors'
@@ -29,12 +31,16 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-const WalletOption = ({ connector, name, icon }) => {
+const WalletOption = ({ connector, name, icon, handleActivation }) => {
     const classes = useStyles();
 
     return (
         <Box mb={2}>
-            <Card variant="outlined" className={classes.option}>
+            <Card
+                variant="outlined"
+                className={classes.option}
+                onClick={() => handleActivation(connector, name)}
+            >
                 <Box display="flex" alignItems="center" p={2}>
                     <img src={icon} alt={name} className={classes.img}/>
                     <Typography className={classes.text}>{name}</Typography>
@@ -45,10 +51,34 @@ const WalletOption = ({ connector, name, icon }) => {
 }
 
 const WalletModal = ({ open, onClose }) => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const { activate } = useWeb3React();
     const classes = useStyles();
 
+    const handleOnClose = () => {
+        setError('');
+        onClose();
+    };
+    const handleActivation = (connector, name) => {
+        setLoading(true)
+        activate(connector, undefined, true)
+            .then(() => {
+                onClose();
+                setError('');
+            })
+            .catch(error => {
+                if (error) {
+                    setError(`Connet ${name} failed.`);
+                }
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+    }
+
     return (
-        <Modal open={open} onClose={onClose}>
+        <Modal open={open} onClose={handleOnClose}>
             <Fade in={open} easing="enter">
                 <Box className={classes.modal}>
                     <Paper elevation={0}>
@@ -57,14 +87,26 @@ const WalletModal = ({ open, onClose }) => {
                                 <SettingsEthernetOutlinedIcon />
                                 <Typography className={classes.text}>Connect to a wallet.</Typography>
                             </Box>
-                            <Box>
-                                {supportedWallet.map((config, idx) => (
+                            <Box mb={3}>
+                                {!loading ? supportedWallet.map((config, idx) => (
                                     <WalletOption
                                         key={`support-wallet-${idx}`}
                                         {...config}
+                                        handleActivation={handleActivation}
                                     />
-                                ))}
+                                )) : (
+                                    <Card
+                                        variant="outlined"
+                                        className={classes.option}
+                                    >
+                                        <Box display="flex" alignItems="center" p={2}>
+                                            <CircularProgress color="grey.500" size={16} />
+                                            <Typography className={classes.text}>Initializing ...</Typography>
+                                        </Box>
+                                    </Card>
+                                )}
                             </Box>
+                            {error && <Typography>{error}</Typography>}
                         </Box>
                     </Paper>
                 </Box>
